ff56fcb11882b040a39f808dc426c6025ccac48e
Carlos-Castillo-Garcia <73341536+Carlos-Castillo-Garcia@users.noreply.github.com>
2020-12-14 17:41:33 +0100

    terminados

--- Proyecto_final_trimestre/src/Trimestre_1/Proyecto_Isma_Carlos2.java
+++ Proyecto_final_trimestre/src/Trimestre_1/Proyecto_Isma_Carlos2.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
  * @author CARLOS
  *
  */
-public class Proyecto_Isma_Carlos1 {
+public class Proyecto_Isma_Carlos2 {
 
 	/**
 	 * @param args
@@ -20,20 +20,16 @@ public class Proyecto_Isma_Carlos1 {
 		
 		int fila;
 		int columna;
-		int pmc;
 		int numerocimas = 0;
 		int cimamasalta = 0;
-		
+		int zona [][] = new int [3][3];
 		
 		System.out.println("las filas:");
 		fila = entrada.nextInt();
 		System.out.println("las columnas:");
 		columna = entrada.nextInt();
-		System.out.println("perimetro de comparacion:");
-		pmc = entrada.nextInt();
 		
 		int mapa [][] = new int [fila][columna];
-		int zona [][] = new int [(pmc*2)+1][(pmc*2)+1];
 		//rellenado de matriz mapa
 		for(int cf = 0;cf < fila; cf++) {
 			for(int cc = 0; cc < columna; cc++) {
@@ -47,47 +43,46 @@ public class Proyecto_Isma_Carlos1 {
 			}
 			System.out.println("");
 		}
-		int z=0;
+		int z = 0;
 		int pf = 0;
 		do {
 			int pc = 0;
 			do {
-				for(int f = (0+pf), fz = 0; f < (pmc*2)+1; f++, fz++) {
- 					for(int c = (0+pc), cz = 0; c < (pmc*2)+1; c++, cz++) {
+				for(int f = (0+pf), fz = 0; f < (pf+3); f++, fz++) {
+ 					for(int c = (0+pc), cz = 0; c < (pc+3); c++, cz++) {
 						zona[fz][cz] = mapa[f][c];
 					}
 				}
 				//comprobar el 1º centro con sus alrederores
 				int m = 0;
-				for(int f = 0 ; f < (pmc*2)+1; f++) {
-					for(int c = 0; c < (pmc*2)+1; c++) {
+				for(int f = 0 ; f < 3; f++) {
+					for(int c = 0; c < 3; c++) {
 						//Si (si sus alrederores son menores en almenos 1 es cima)
-						if(zona[pmc][pmc] - zona[f][c] > 0 ) {
-							
+						if(zona[1][1] - zona[f][c] > 0 ) {
 						}else {//Si no (no es cima)
 							m++;
 						}
 					}
 				}
-
-//				if(m==1) {
-//					//sumamos contador de cimas 
-//					numerocimas++;
-//					//comprobar si es valor maximo
-//					if(zona[pmc][pmc] == 9) {
-//						//sumar contador de cimas mayores
-//						cimamasalta++;
-//					}//si no es valor maximo solo contarlo
-//				}
-//			//rellenado de las siguentes zonas
+				if(m==1) {
+					//sumamos contador de cimas 
+					numerocimas++;
+					//comprobar si es valor maximo
+					if(zona[1][1] == 9) {
+						//sumar contador de cimas mayores
+						cimamasalta++;
+					}//si no es valor maximo solo contarlo
+				}
+			//rellenado de las siguentes zonas
 				pc++;
-				if(pc == columna-(pmc*2)) {
+				if(pc == columna-2) {
 					pf++;
 				}	
 				System.out.println(z);
 				z++;
-			}while(pc < columna-(pmc*2) );
-		}while(pf < fila-(pmc*2));
+			}while(pc < columna-2 );
+		}while(pf < fila-2);
+		
 		System.out.println("el numero de cimas es:"+numerocimas);
 		System.out.println("el numero de cimas mas altas es:"+cimamasalta);
 	
